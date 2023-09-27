4feb1fce3d714434ed1997796f3390e912a2ad39
kwonjoonhyung <rnjswnsgud0406@naver.com>
2023-09-28 04:06:15 +0900

    아

---
+++ src/main/java/KANDG/Lotto365/Lotto365Application.java
@@ -0,0 +1,13 @@
+package KANDG.Lotto365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lotto365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lotto365Application.class, args);
+	}
+
+}
---
+++ src/test/java/KANDG/Lotto365/Lotto365ApplicationTests.java
@@ -0,0 +1,13 @@
+package KANDG.Lotto365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lotto365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
